11613e382cb4bd8a3dad1258dcbfc7b730ec5e61
Salma Ayuningtyas <ayuningtyas.ayusalma@gmail.com>
2025-10-17 14:21:34 +0700

    Initial Commit

---
+++ PiscumPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiscumPhotos
+//
+//  Created by Salma Ayuningtyas on 10/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PiscumPhotosTests/PiscumPhotosTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiscumPhotosTests.swift
+//  PiscumPhotosTests
+//
+//  Created by Salma Ayuningtyas on 10/17/25.
+//
+
+import Testing
+@testable import PiscumPhotos
+
+struct PiscumPhotosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
